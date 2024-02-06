cc87184028778d34d45b6ddc112df9a64c200257
JadeSawyer <chen.pinyu@hotmail.com>
2024-02-06 09:07:04 -0500

    Script

--- Assets/Scripts/Nuages.cs
+++ Assets/Scripts/Nuages.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Nuages : MonoBehaviour
 {
+    private float _positionX;
+    private float _positionY;
+    private float _positionZ;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _positionX = transform.position.x;
+        _positionY = transform.position.y;
+        _positionZ = transform.position.z;
+
+        BougeNuage();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void BougeNuage(){
+        transform.position = new Vector3(50, 50, 100);
     }
 }
