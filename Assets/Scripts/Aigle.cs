df56317aebbd1fd193cfa2d4888a1160292da4e1
JadeSawyer <chen.pinyu@hotmail.com>
2024-02-06 10:00:49 -0500

    rigidbody

--- Assets/Scripts/Aigle.cs
+++ Assets/Scripts/Aigle.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Aigle : MonoBehaviour
 {
+    private Rigidbody _rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        BougeAigle();
+    }
+
+    void BougeAigle(){
+        _rb.velocity = Vector3.forward * -1;
     }
 }
