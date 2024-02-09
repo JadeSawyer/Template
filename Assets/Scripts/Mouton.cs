ae8ca2558d9ce1e3c0fc1acefdb694e4decb230e
JadeSawyer <chen.pinyu@hotmail.com>
2024-02-09 12:00:56 -0500

    Script mouton

--- Assets/Scripts/Mouton.cs
+++ Assets/Scripts/Mouton.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Mouton : MonoBehaviour
 {
     [SerializeField] private GameObject _zoneDanger;
+    [SerializeField] private GameObject _parachute;
+
+    private Rigidbody _rbMouton;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rbMouton = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -18,8 +21,19 @@ public class Mouton : MonoBehaviour
         
     }
 
+//---------------------------------------------------------------------
+
     private void OnTriggerEnter(Collider other)
     {
-        
+        // Debug.Log(other.gameObject.tag);
+
+        if(other.gameObject.tag == _zoneDanger.gameObject.tag) {
+            // Debug.Log(other.gameObject.tag);
+            _parachute.SetActive(true);
+            _rbMouton.useGravity = false;
+            _rbMouton.drag += 1;
+        }
     }
+
+//---------------------------------------------------------------------
 }
